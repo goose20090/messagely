c664ebf4f4402dc28cfb6f46646b22dfb2fa7c72
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-03-13 11:45:20 +0000

    added rudimentary login form to App home page for building auth

---
+++ client/src/components/LoginDraft.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function LoginDraft(){
+
+    return(
+        <form>
+            <label>Username</label>
+            <input type={"text"}/>
+            <label>Password</label>
+            <input type={"text"}/>
+            <input type={"submit"}/>
+        </form>
+    )
+}
+
+export default LoginDraft;
