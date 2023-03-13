cf0e0f50cc02e41271706f54b805edeb8df2507a
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-03-13 10:51:59 +0000

    corrected associations formatting and added basic seed data

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
-    belongs_to :users
-    belongs_to :conversations
+    belongs_to :user
+    belongs_to :conversation
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+    User.create!(
+      username: Faker::Internet.username,
+      password: Faker::Internet.password,
+      avatar_url: Faker::Avatar.image
+    )
+  end
+  
+  # create 2 conversations
+  2.times do
+    Conversation.create!
+  end
+  
+  # create messages for each conversation
+  Conversation.all.each do |conversation|
+    # create 10 messages for each conversation
+    10.times do
+      Message.create!(
+        content: Faker::Lorem.sentence,
+        user_id: User.all.sample.id,
+        conversation_id: conversation.id
+      )
+    end
+  end
