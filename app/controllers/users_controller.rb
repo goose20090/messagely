e7a2ce9e58acd990415bfa574358b1f3c10f8989
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-03-13 17:08:57 +0000

    reformatted user controller, using a rescue_from clause to handle some errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
     def index
         users = User.all
@@ -7,27 +8,22 @@ class UsersController < ApplicationController
 
     def show
         user = User.find_by(id: session[:user_id])
-        if user
-            render json: user
-        else 
-            render json: {error: "Not authorised"}, status: :unauthorized
-        end
+        render json: user
     end
 
     def create
-        user = User.create(user_params)
-
-        if user.valid?
-            session[:user_id] = user.id
-            render json: user, status: :created
-        else
-            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-        end
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
     end
 
     private
     
     def user_params
         params.permit(:username, :password, :password_confirmation)
+    end
+
+    def render_unprocessable_entity_response invalid
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
     end
 end
