8544efe03f0b30d611d5c41f2a91b9113b453553
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-03-13 11:51:13 +0000

    form is now controlled

--- client/src/components/LoginDraft.js
+++ client/src/components/LoginDraft.js
@@ -1,13 +1,22 @@
 import React from "react"
+import { useState } from "react";
 
 function LoginDraft(){
 
+    const [formData, setFormData] = useState({username: "", password: ""})
+
+    function handleChange(e){
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
     return(
         <form>
-            <label>Username</label>
-            <input type={"text"}/>
-            <label>Password</label>
-            <input type={"text"}/>
+            <label>Username: </label>
+            <input name = "username" type={"text"} onChange = {handleChange} value = {formData.username}/>
+            <label>Password: </label>
+            <input name = "password" type={"text"} onChange = {handleChange} value = {formData.password}/>
             <input type={"submit"}/>
         </form>
     )
