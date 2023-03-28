e7f31c3b26d9d24d851f1715ff29011c0b740eb1
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-03-28 12:42:40 +0100

    modified current_user method, to see if that fixes error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::API
 
   private
   def current_user
-    User.find(session[:user_id])
+    if session[:user_id].present?
+      User.find_by(id: session[:user_id])
+    end
   end
 end
