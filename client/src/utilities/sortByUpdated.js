1e2cd0c65e87fc880656857539f5a6039b527a8a
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-04-05 21:15:54 +0100

    also fixed conversation sorting function

--- client/src/utilities/sortByUpdated.js
+++ client/src/utilities/sortByUpdated.js
@@ -2,9 +2,9 @@
 
 export function sortByUpdatedAt(conversations) {
   return conversations.sort((a, b) => {
-    const dateA = new Date(a.updated_at);
-    const dateB = new Date(b.updated_at);
+    const lastMessageA = a.messages[a.messages.length - 1].created_at;
+    const lastMessageB = b.messages[b.messages.length - 1].created_at;
 
-    return dateB - dateA;
+    return new Date(lastMessageB) - new Date(lastMessageA);
   });
 }
